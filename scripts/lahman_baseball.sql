bcfb85eb09fff9e392b53bdc7fbf9692cde297ec
Melanie Wesson <melaniedwesson@gmail.com>
2023-03-04 10:44:03 -0600

    working on 7

--- scripts/lahman_baseball.sql
+++ scripts/lahman_baseball.sql
@@ -117,6 +117,57 @@ ORDER BY success_percentage DESC
 
 -- 7.  From 1970 – 2016, what is the largest number of wins for a team that did not win the world series? What is the smallest number of wins for a team that did win the world series? Doing this will probably result in an unusually small number of wins for a world series champion – determine why this is the case. Then redo your query, excluding the problem year. How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
 
+--Teams table- wins/losses WSWin
+
+SELECT name, yearid, w
+FROM teams
+WHERE wswin <> 'Y'
+AND yearID BETWEEN 1970 AND 2016
+GROUP BY name, yearid, w
+ORDER BY w DESC
+
+--Answer 1: Seattle Mariners won 116 games in 2001
+
+SELECT name, yearid, w
+FROM teams
+WHERE wswin = 'Y'
+AND yearID BETWEEN 1970 AND 2016
+GROUP BY name, yearid, w
+ORDER BY w ASC
+
+--Answer 2: Los Angeles Dodgers won 63 games in 1981. According to wikipedia this was due to a players' strike, which split the season into two halves
+
+SELECT name, yearid, w
+FROM teams
+WHERE wswin = 'Y'
+AND yearID BETWEEN 1970 AND 2016
+AND yearID <> 1981
+GROUP BY name, yearid, w
+ORDER BY w ASC
+
+--Answer 3: taking out the 1981 series, the lowest wins for a WS winner is 83 games for the St. Louis Cardinals in 2006
+--How often from 1970 – 2016 was it the case that a team with the most wins also won the world series? What percentage of the time?
+
+SELECT name, teamid, yearid, wswin, MAX(w) OVER
+	(PARTITION BY yearid) AS most_wins_per_year
+FROM (SELECT name, teamid, yearid, w, wswin
+FROM teams
+WHERE wswin = 'Y') AS winner
+WHERE yearid BETWEEN 1970 AND 2016
+AND teamid IN 
+
+with WCWinners AS
+	(Select name, teamid, yearid, w, wswin
+	FROM teams
+	WHERE wswin = 'Y')
+	
+SELECT name, yearid,
+	MAX(w) OVER (PARTITION BY yearid) AS most_wins_per_year
+FROM teams
+WHERE teamid IN WCWinners
+
+
+
 
 -- 8. Using the attendance figures from the homegames table, find the teams and parks which had the top 5 average attendance per game in 2016 (where average attendance is defined as total attendance divided by number of games). Only consider parks where there were at least 10 games played. Report the park name, team name, and average attendance. Repeat for the lowest 5 average attendance.
 
