efb346b938ff1d16c68a5c5ee2d40647f7b88a62
Melanie Wesson <melaniedwesson@gmail.com>
2023-03-07 20:16:55 -0600

    finished 10

--- scripts/lahman_baseball.sql
+++ scripts/lahman_baseball.sql
@@ -206,6 +206,23 @@ WHERE a.playerid IN
 
 -- 10. Find all players who hit their career highest number of home runs in 2016. Consider only players who have played in the league for at least 10 years, and who hit at least one home run in 2016. Report the players' first and last names and the number of home runs they hit in 2016.
 
+with maxhr AS (
+SELECT playerid, yearid, MAX(hr) AS max
+FROM batting
+GROUP BY playerid, yearid
+HAVING MAX(hr) >= 1
+ORDER BY yearid)
+
+SELECT namefirst, namelast, maxhr.max
+FROM people
+JOIN maxhr
+USING (playerid)
+WHERE yearid = 2016
+AND people.debut :: date < '2006-01-01'
+ORDER BY max DESC;
+
+
+
 
 -- **Open-ended questions**
 
